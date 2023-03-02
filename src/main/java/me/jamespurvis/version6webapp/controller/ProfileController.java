29308391bf670671512d3c6ca17236af7b3dc542
James Purvis <110248591+JamesPurvis@users.noreply.github.com>
2023-03-02 00:18:32 -0700

    avatar details now shows logged in user info.

--- src/main/java/me/jamespurvis/version6webapp/controller/ProfileController.java
+++ src/main/java/me/jamespurvis/version6webapp/controller/ProfileController.java
@@ -1,12 +1,24 @@
 package me.jamespurvis.version6webapp.controller;
 
 
+import me.jamespurvis.version6webapp.models.Avatar;
+import me.jamespurvis.version6webapp.services.AvatarService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class ProfileController {
+
+    @Autowired
+    private AvatarService avatarService;
 
     @RequestMapping("/profile")
     @PreAuthorize("isAuthenticated()")
@@ -15,8 +27,23 @@ public class ProfileController {
     }
 
     @RequestMapping("/profile_avatar")
-    @PreAuthorize("isAuthenticated()")
-    public String showUpdatePage() {
+    @PreAuthorize("isAuthenticated")
+    public String showUpdatePage(Model model) {
+      UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Optional<Avatar> avatar = avatarService.findByUsername(userDetails.getUsername());
+
+        model.addAttribute("mission", avatar.get().getMission());
+        model.addAttribute("credits", avatar.get().getCredits());
+        model.addAttribute("role", avatar.get().getUser_role());
+        model.addAttribute("email", avatar.get().getEmail());
+        model.addAttribute("figure", avatar.get().getFigure());
+        model.addAttribute("id", avatar.get().getId());
+        model.addAttribute("film", avatar.get().getFilm());
+        model.addAttribute("tickets", avatar.get().getTickets());
+        model.addAttribute("persistent_message", avatar.get().getPersistent_message());
+        model.addAttribute("pool_figure", avatar.get().getPool_figure());
+        model.addAttribute("sex", avatar.get().getSex());
+
         return "profile_avatar";
     }
 }
